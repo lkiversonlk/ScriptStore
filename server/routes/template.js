c99f90c79dedfc879f82e088c059614b54b8fb52
lkiversonlk <city.of.beijing@gmail.com>
2016-04-25 11:06:21 +0800

    add js template rendering

--- server/routes/template.js
+++ server/routes/template.js
@@ -1,44 +1,53 @@
 /**
  * Created by liukan on 4/19/16.
+ *
+ *
+ * there are 3 entries:
+ *   1. standard
+ *    standard is the basic script which should be put directly in advertiser's website
+ *   2. dynamic
+ *    for each dynamic type ("viewItem,  viewList, viewSearch, viewChannel..."
+ *    there will be a template for rendering
+ *
+ * entry format:
+ * standard:
+ *   /template/standard/[js, as2, as3, img]
+ *    return the corresponding version of the standard code
+ *
+ *   /template/template/[event]/[js, as2, as3, img]
+ *    return the corresponding version of the version code
  */
+
 var express = require("express"),
     router = express.Router();
 var path = require("path");
 var fs = require("fs");
-var dots = require("dot");
 var swig = require("swig");
+var logger = require("../log").getLogger("routes.template");
 var SErrors = require("../errors");
 
-var templates = {};
+var templatePath = path.join(__dirname, "templates");
 
-var entris = [
-    "standard",
-    "action",
-    "conversion"
-];
+function render(filePath, req, res, next) {
+    var fac = null;
+    try {
+        fac = swig.compileFile(filePath);
+    }catch (error){
+        logger.log("error", "template rendering error: " + error.message || error);
+        return next(SErrors.PathInvalidError);
+    }
+    req.SsiData.result = fac(req.body);
+    return next();
+}
 
-var types = [
-    "js",
-    "img",
-    "as2",
-    "as3"
-];
+router.post('/standard/:type', function (req, res, next) {
+    var filePath = path.join(templatePath, req.params.type, "standard");
+    render(filePath, req, res, next);
+});
 
-entris.forEach(function (entry) {
-    types.forEach(function (type) {
-       router.get("/" + entry + "/" + type, function (req, res, next) {
-           if(!templates[entry]){
-               templates[entry] = {}
-           }
-           if(!templates[entry][type]){
-               var filePath = path.join(__dirname, "templates", type, entry);
-               templates[entry][type] = swig.compileFile(filePath);
-           }
-           req.SsiData = {};
-           req.SsiData.result = templates[entry][type](req.query);
-           return next();
-       })
-    });
+router.post('/event/:event/:type', function (req, res, next) {
+    var filePath = path.join(templatePath, req.params.type, req.params.event);
+    render(filePath, req, res, next);
 });
 
 module.exports = router;
