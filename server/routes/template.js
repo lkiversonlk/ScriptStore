868c17c84158c35ae154e762f5fdc3fb4ea4c914
lkiversonlk <city.of.beijing@gmail.com>
2016-05-04 09:51:16 +0800

    Merge branch 'master' of gitlab.ipinyou.com:kan.liu/pyscript
    # Conflicts:
    #	server/routes/templates/as2/addCart
    #	server/routes/templates/as2/collect
    #	server/routes/templates/as2/conversion
    #	server/routes/templates/as2/custom
    #	server/routes/templates/as2/leads
    #	server/routes/templates/as2/order
    #	server/routes/templates/as2/register
    #	server/routes/templates/as2/viewActivity
    #	server/routes/templates/as2/viewCart
    #	server/routes/templates/as2/viewChannel
    #	server/routes/templates/as2/viewItemInID
    #	server/routes/templates/as2/viewList
    #	server/routes/templates/as2/viewSearch
    #	server/routes/templates/as2/viewUserIndex
    #	server/routes/templates/as3/addCart
    #	server/routes/templates/as3/collect
    #	server/routes/templates/as3/conversion
    #	server/routes/templates/as3/custom
    #	server/routes/templates/as3/leads
    #	server/routes/templates/as3/order
    #	server/routes/templates/as3/register
    #	server/routes/templates/as3/standard
    #	server/routes/templates/as3/viewCart
    #	server/routes/templates/as3/viewChannel
    #	server/routes/templates/as3/viewHome
    #	server/routes/templates/as3/viewItemInID
    #	server/routes/templates/as3/viewList
    #	server/routes/templates/as3/viewSearch
    #	server/routes/templates/as3/viewUserIndex

--- server/routes/template.js
+++ server/routes/template.js
@@ -28,7 +28,9 @@ var SErrors = require("../errors");
 
 var templatePath = path.join(__dirname, "templates");
 
-function render(filePath, req, res, next) {
+var categories = fs.readdirSync(templatePath);
+
+function render(filePath, req, res, callback) {
     var fac = null;
     try {
         fac = swig.compileFile(filePath);
@@ -40,14 +42,97 @@ function render(filePath, req, res, next) {
     return next();
 }
 
-router.post('/standard/:type', function (req, res, next) {
-    var filePath = path.join(templatePath, req.params.type, "standard");
-    render(filePath, req, res, next);
-});
+/**
+ * fetch default data in /templates/[category]/default/[model]
+ * @param category
+ * @param type
+ * @param model
+ * @param callback
+ */
+function loadDefault(category, type, model, callback) {
+    var defaultPath = path.join(templatePath, category, "default", model);
+    fs.stat(defaultPath, function (err, stats) {
+        if(err){
+            logger.log("debug", "read data " + defaultPath + " failed: " + err.message);
+            return return callback(err);
+        }else{
+            if(stats.isFile()){
+                return fs.readFile(defaultPath, callback);
+            } else {
+                logger.log("debug", "read data " + defaultPath + " failed, this is not file");
+                return callback(SErrors.LogicError("template default is not file"));
+            }
+        }
+    })
+}
 
-router.post('/event/:event/:type', function (req, res, next) {
-    var filePath = path.join(templatePath, req.params.type, req.params.event);
-    render(filePath, req, res, next);
+/**
+ * fetch data belong to /templates/[category]/[type]/[model]
+ * @param category
+ * @param type
+ * @param model
+ * @param callback
+ */
+function fetchData(category, type, model, callback) {
+    var path = path.join(templatePath, category, type, model);
+
+    fs.stat(path, function (err, stats) {
+        if(err){
+            //file not existed, find the default
+            logger.log("debug", "read data " + path + " failed, load default");
+            return loadDefault(category, type, model, callback);
+        }else{
+            if(stats.isFile()){
+                return fs.readFile(path, callback);
+            } else {
+                logger.log("debug", path + " is not file, load default");
+                return loadDefault(category, type, model, callback);
+            }
+        }
+    })
+}
+
+/**
+ * the templates folder path:
+ * templates:
+ *    /tags
+ *       [as2, as3, img, js, ]
+ *    /desc
+ *       [as2, as3, img, js, default]
+ *    /desc-eng
+ *       [as2, as3, img, js, default]
+ */
+router.post('/:type/:model', function (req, res, next) {
+    var type = req.params.type;     //js, as, as2, img
+    var model = req.params.model;   //viewHome, view...
+
+    var codePath = path.join(templatePath, "tags", type, model);
+
+    var ret = {};
+
+    async.map(
+        categories,
+        function (category, callback) {
+            fetchData(category, type, model, function (err, data) {
+                if(err){
+                    logger.log("fail to load data of category " + category + ": " + err.message);
+                    return callback();
+                } else {
+                    if(category == "tags"){
+                        var fac = swig.compile(data);
+                        ret[category] = fac(req.body);
+                    }else{
+                        ret[category] = data;
+                    }
+                    return callback();
+                }
+            })
+        },
+        function (err, results) {
+            req.SsiData.Result = ret;
+            return next();
+        }
+    );
 });
 
 module.exports = router;
