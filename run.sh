c98e4c920e8876f41eb1c74883e000726ad16e63
lkiversonlk <city.of.beijing@gmail.com>
2016-05-20 13:14:00 +0800

    增加运行脚本

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NODE_ENV=production PORT=3000 nohup node bin/www > logs/N3000.log &
-NODE_ENV=proudction PORT=3001 nohup node bin/www > logs/N3001.log &
+NODE_ENV=production PORT=3000 nohup node bin/www > /dev/null &
+NODE_ENV=proudction PORT=3001 nohup node bin/www > /dev/null &
