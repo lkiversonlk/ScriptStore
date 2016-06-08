7907c20f6f42a7111a9c84d9dc82fc7e62c79a4f
lkiversonlk <city.of.beijing@gmail.com>
2016-06-09 00:04:13 +0800

    v1.0 update: 1. 修改config.yaml的格式,db变成string数组,增加concurrency配置                      2. 关闭一些失效的Automation: 主要原因是cache加入导致生效时间延迟.                      3. 在app.js中控制并发数量.                      4. 将静态文件功能后移,免得影响脚本模块功能

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var winston = require("winston");
 
-
 var restRouter = require('./server/routes/rest');
 var manage = require("./server/routes/manage");
 var middlewares = require("./server/middlewares");
-//var users = require('./routes/users');
-
 
 var app = express();
 
@@ -24,6 +21,7 @@ var configuration = null;
 var configurationSchema = require("./configs/configSchema.json");
 var yaml = require("js-yaml");
 var fs = require("fs");
+require("http").globalAgent.maxSockets = Infinity;
 
 try{
     configuration = yaml.safeLoad(fs.readFileSync("./configs/config.yaml", "utf-8"));
@@ -31,30 +29,12 @@ try{
     console.log("fail to load configuration " + error.message);
     process.exit(-1);
 }
-/*
-try{
-    configuration = require("./configs/test.json");
-    console.log("under test environment");
-}catch (error){
-    console.log("not under test environment");
-}
-*/
-
 
 var jsen = require("jsen");
-
 var validate= jsen(configurationSchema);
 if(validate(configuration)){
-    console.log("configuration validation passed");
     app.set("SsiConfiguration", configuration);
-
-    if(configuration.app){
-        if(configuration.app.log){
-            if(configuration.app.log.level){
-                winston.level = configuration.app.log.level;
-            }
-        }
-    }
+    winston.level = configuration.app.log.level;
 }else{
     console.log("configuration file invalid : ");
     validate.errors.forEach(function(error){
@@ -64,16 +44,20 @@ if(validate(configuration)){
 }
 
 var mongoose = require("mongoose");
-var connectString = configuration.db.map(function(db){
-    return db.connectString
-}).join(",");
+var connectString = configuration.db.join(",");
 
-var options = {};
-options.server = {};
-options.replset = {};
-options.server.socketOptions =
-    options.replset.socketOptions =
-    {keepAlive : 120};
+var options = {
+    server : {
+        socketOptions : {
+            keepAlive : 50
+        }
+    },
+    replset : {
+        socketOptions : {
+            keepAlive : 50
+        }
+    }
+};
 
 mongoose.connect(connectString, options);
 
@@ -85,31 +69,31 @@ winston.add(require("winston-daily-rotate-file"),
         maxsize: 1024 * 1024 * 1024 * 1
     });
 
+/*
 process.on('uncaughtException', function (err) {
     winston.log("error", err.stacktrace);
     process.exit(-1);
 });
+*/
+var onFinished = require("on-finished");
+var conLimit = configuration.app.concurrency;
+var concurrency = 0;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('tiny'));
+app.use(function (req, res, next) {
+    if(concurrency > conLimit){
+        return res.sendStatus(404);
+    }
+    concurrency ++;
+    onFinished(res, function (err, res) {
+        concurrency --;
+    });
+    return next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 var SsiData = require("./server/SsiData");
-
-/**
- * disable Nagle algorithm to speed up traffic
- */
-
-app.use(function(req, res, next){
-    req.connection.setNoDelay(true);
-    next();
-});
-
-require("http").globalAgent.maxSockets = Infinity;
-
 
 app.get("/", function(req, res, next){
     res.render("index", {title : "Smart Pixel"});
@@ -126,6 +110,10 @@ app.use("/template", template, middlewares.presentation.present, middlewares.pre
 app.use(middlewares.parameters);
 app.use('/rest', restRouter);
 app.use('/manage', manage);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(middlewares.operation);
 app.use(middlewares.presentation.present);
 app.use(middlewares.presentation.presentError);
